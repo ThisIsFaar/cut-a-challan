bf528e73481075900aa15076bf24acf014fc13c0
thisisfaar <thisisfarhan99@gmail.com>
2021-11-23 22:40:20 +0530

    testing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+var path = require("path");
+var serveStatic = require("serve-static");
+
 const app = express();
 const port = 3000;
 
-app.use("/static", express.static("public"));
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// app.use("/static", express.static("public"));
+app.use(serveStatic(path.join(__dirname, "public")));
+app.listen(port);
