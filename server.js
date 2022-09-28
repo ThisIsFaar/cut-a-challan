2dab08dbf4f1b5d8f32e843fd094462010ad916e
thisisfaar <thisisfarhan99@gmail.com>
2022-09-29 01:25:17 +0530

    server fixed

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+const path = require('path');
 
-app.get('/', (req, res) => {
-  //   res.send("Hello World!");
-  app.use(express.static('public'));
+//where we expose files or directories for public use
+// app.use(express.static(path.join(__dirname)));
+app.use(express.static('public'));
+
+//where we set our routes
+app.get('/', function (req, res) {
+  res.sendFile('public/index.html');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(process.env.PORT || 3000);
